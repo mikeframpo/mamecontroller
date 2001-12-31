6733dc4196362cd0ea1d37b979db352f85f00be0
mikeframpo <mikeframpo@gmail.com>
2002-01-01 06:25:34 +1300

    Rewrote the firmware from scratch in an effort to fix existing bugs. Initial reports showing positive results.

---
+++ firmware2/main.c
@@ -0,0 +1,229 @@
+
+#include "main.h"
+
+#include <avr/pgmspace.h>   /* required by usbdrv.h */
+#include "usbdrv.h"
+
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/interrupt.h>
+#include <string.h>
+
+/* USB report descriptor (length is defined in usbconfig.h)
+   This has been changed to conform to the USB keyboard boot protocol */
+char usbHidReportDescriptor[USB_CFG_HID_REPORT_DESCRIPTOR_LENGTH] PROGMEM = {
+	0x05, 0x01,            // USAGE_PAGE (Generic Desktop)
+	0x09, 0x06,            // USAGE (Keyboard)
+	0xa1, 0x01,            // COLLECTION (Application)
+	0x05, 0x07,            //   USAGE_PAGE (Keyboard)
+	
+	0x95, 0x08,            //   REPORT_COUNT (6)
+	0x75, 0x08,            //   REPORT_SIZE (8)
+	0x15, 0x00,            //   LOGICAL_MINIMUM (0)
+	0x25, 0x65,            //   LOGICAL_MAXIMUM (101)
+	0x05, 0x07,            //   USAGE_PAGE (Keyboard)
+	
+	0x19, 0x00,            //   USAGE_MINIMUM (Reserved (no event indicated))
+	0x29, 0x65,            //   USAGE_MAXIMUM (Keyboard Application)
+	0x81, 0x00,            //   INPUT (Data,Ary,Abs)
+	0xc0                   // END_COLLECTION
+};
+
+#define REPORT_BUF_LEN 8
+static uint8_t reportBuffer[REPORT_BUF_LEN];    /* buffer for HID reports */
+static uint8_t idleRate;           /* in 4 ms units */
+static uint8_t protocolVer = 1;    /* 0 is boot protocol, 1 is report protocol */
+static uint8_t expectReport = 0;
+
+uint8_t usbFunctionSetup(uint8_t data[8]) {
+	usbRequest_t *rq = (void *)data;
+	usbMsgPtr = reportBuffer;
+
+	if ((rq->bmRequestType & USBRQ_TYPE_MASK) != USBRQ_TYPE_CLASS)
+		return 0;
+
+	switch (rq->bRequest) {
+		case USBRQ_HID_GET_IDLE:
+			usbMsgPtr = &idleRate;
+			return 1;
+		case USBRQ_HID_SET_IDLE:
+			idleRate = rq->wValue.bytes[1];
+			return 0;
+		case USBRQ_HID_GET_REPORT:
+			return sizeof(reportBuffer);
+		case USBRQ_HID_SET_REPORT:
+			if (rq->wLength.word == 1)
+				expectReport = 1;
+			return expectReport == 1 ? 0xFF : 0;
+		case USBRQ_HID_GET_PROTOCOL:
+			if (rq->wValue.bytes[1] < 1)
+				protocolVer = rq->wValue.bytes[1];
+			return 0;
+		case USBRQ_HID_SET_PROTOCOL:
+			usbMsgPtr = &protocolVer;
+			return 1;
+		default:
+			return 0;
+	}
+}
+
+#define NUM_BUTTONS 12
+button_t buttons[NUM_BUTTONS];
+
+static inline bool_t getButtonState(button_t* button) {
+    switch(button->port) {
+        case PORT_A:
+            return (PINA & button->pin) == 0;
+        case PORT_B:
+            return (PINB & button->pin) == 0;
+        case PORT_C:
+            return (PINC & button->pin) == 0;
+        case PORT_D:
+            return (PIND & button->pin) == 0;
+        default:
+            return FALSE;
+    }
+}
+
+static inline void resetCycles(button_t* button) {
+    if (button->debouncedState) {
+        button->cyclesRemaining = RELEASED_CYCLES;
+    } else {
+        button->cyclesRemaining = DEPRESSED_CYCLES;
+    }
+}
+
+void debounceButtons(uint8_t* reportBuffer, int8_t* numPressed, int8_t* numChanged) {
+    int iButton;
+    *numPressed = 0;
+    *numChanged = 0;
+    for (iButton = 0; iButton < NUM_BUTTONS; iButton++) {
+        
+        button_t* button = &buttons[iButton];
+        bool_t rawState = getButtonState(button);
+        
+        if (rawState == button->debouncedState) {
+            resetCycles(button);
+        } else {
+            button->cyclesRemaining--;
+            if (button->cyclesRemaining == 0) {
+                button->debouncedState = rawState;
+                resetCycles(button);
+                (*numChanged)++;
+            }
+        }
+        
+        if (button->debouncedState) {
+            reportBuffer[(*numPressed)++] = button->key;
+        }
+    }
+}
+
+void addButton(port_t port, uint8_t pin, keycode_t key, int* index) {
+    
+    buttons[*index].port = port;
+    buttons[*index].pin = pin;
+    buttons[*index].debouncedState = FALSE;
+    buttons[*index].cyclesRemaining = RELEASED_CYCLES;
+    buttons[*index].key = key;
+    
+    //pullup
+    switch(port) {
+        case PORT_A:
+            PORTA |= pin;
+            break;
+        case PORT_B:
+            PORTB |= pin;
+            break;
+        case PORT_C:
+            PORTC |= pin;
+            break;
+        case PORT_D:
+            PORTD |= pin;
+            break;
+        default:
+            break;
+    }
+    
+    (*index)++;
+}
+
+void initButtons() {
+    
+    int index = 0;
+
+    addButton(PORT_A, P2_A, KEY_G, &index);
+    addButton(PORT_A, P2_B, KEY_H, &index);
+    addButton(PORT_A, P2_C, KEY_J, &index);
+    addButton(PORT_A, P2_D, KEY_B, &index);
+    addButton(PORT_A, P2_E, KEY_N, &index);
+    addButton(PORT_A, P2_F, KEY_M, &index);
+    
+    addButton(PORT_C, P2_A, KEY_A, &index);
+    addButton(PORT_C, P2_B, KEY_S, &index);
+    addButton(PORT_C, P2_C, KEY_D, &index);
+    addButton(PORT_C, P2_D, KEY_Z, &index);
+    addButton(PORT_C, P2_E, KEY_X, &index);
+    addButton(PORT_C, P2_F, KEY_C, &index);
+}
+
+int main(void) {
+
+    DDRD |= RED_LED;
+    
+    wdt_enable(WDTO_1S);
+    /* Even if you don't use the watchdog, turn it off here. On newer devices,
+     * the status of the watchdog (on/off, period) is PRESERVED OVER RESET!
+     */
+	 
+    /* RESET status: all port bits are inputs without pull-up.
+     * That's the way we need D+ and D-. Therefore we don't need any
+     * additional hardware initialization.
+     */
+    usbInit();
+    usbDeviceDisconnect();  /* enforce re-enumeration, do this while interrupts are disabled! */
+    uint8_t i = 0;
+    while(--i){             /* fake USB disconnect for > 250 ms */
+        wdt_reset();
+        _delay_ms(1);
+    }
+    usbDeviceConnect();
+
+    GICR |= IVCE;
+    GICR |= IVSEL;
+    
+    sei();
+    
+    TCCR0 = 0x5;
+    TCNT0 = 0;
+    memset(reportBuffer, 0, sizeof(reportBuffer));
+    
+    initButtons();
+    
+    while(1) {
+        wdt_reset();
+        usbPoll();
+        
+        if (TCNT0 > 58) {
+            TCNT0 = 0;
+            
+            int8_t numPressed;
+            int8_t numChanged;
+            debounceButtons(reportBuffer, &numPressed, &numChanged);
+            
+            //if (numPressed != 0 && numChanged != 0) {
+            //    if (PORTD & RED_LED) {
+            //        PORTD &= ~RED_LED;
+            //    } else {
+            //        PORTD |= RED_LED;
+            //    }
+            //}
+        }
+        
+        if(usbInterruptIsReady()) {
+            usbSetInterrupt(reportBuffer, sizeof(reportBuffer));
+            memset(reportBuffer, 0, REPORT_BUF_LEN);
+        }
+    }
+}
---
+++ firmware2/main.h
@@ -0,0 +1,168 @@
+
+#ifndef DEF_MAIN_H
+#define DEF_MAIN_H
+
+#include <inttypes.h>
+
+#define TRUE (0 == 0)
+#define FALSE (1 == 0)
+
+#define RED_LED (1 << 6) //portD
+
+#define P1_A (1 << 1) //red
+#define P1_B (1 << 3) //yellow
+#define P1_C (1 << 5) //blue
+#define P1_D (1 << 2) //orange
+#define P1_E (1 << 4) //green
+#define P1_F (1 << 6) //purple
+
+#define P2_A (1 << 2) //blue
+#define P2_B (1 << 4) //yellow
+#define P2_C (1 << 6) //red
+#define P2_D (1 << 1) //purple
+#define P2_E (1 << 3) //green
+#define P2_F (1 << 5) //orange
+
+#define DEPRESSED_CYCLES 2
+#define RELEASED_CYCLES 4
+
+typedef enum {
+    PORT_A,
+    PORT_B,
+    PORT_C,
+    PORT_D,
+} port_t;
+
+typedef uint8_t bool_t;
+
+/* The USB keycodes are enumerated here - the first part is simply
+   an enumeration of the allowed scan-codes used for USB HID devices */
+typedef enum keycodes {
+  KEY__=0,
+  KEY_errorRollOver,
+  KEY_POSTfail,
+  KEY_errorUndefined,
+  KEY_A,        // 4
+  KEY_B,
+  KEY_C,
+  KEY_D,
+  KEY_E,
+  KEY_F,
+  KEY_G, 
+  KEY_H,
+  KEY_I,
+  KEY_J,
+  KEY_K,
+  KEY_L,
+  KEY_M,        // 0x10
+  KEY_N,
+  KEY_O,
+  KEY_P,
+  KEY_Q, 
+  KEY_R,
+  KEY_S,
+  KEY_T,
+  KEY_U,
+  KEY_V,
+  KEY_W,
+  KEY_X,
+  KEY_Y,
+  KEY_Z,
+  KEY_1,
+  KEY_2,
+  KEY_3,        // 0x20
+  KEY_4,
+  KEY_5,
+  KEY_6,
+  KEY_7,
+  KEY_8,
+  KEY_9,
+  KEY_0,        // 0x27
+  KEY_enter,
+  KEY_esc,
+  KEY_bckspc,   // backspace
+  KEY_tab,
+  KEY_spc,      // space
+  KEY_minus,    // - (and _)
+  KEY_equal,    // = (and +)
+  KEY_lbr,      // [
+  KEY_rbr,      // ]  -- 0x30
+  KEY_bckslsh,  // \ (and |)
+  KEY_hash,     // Non-US # and ~
+  KEY_smcol,    // ; (and :)
+  KEY_ping,     // ' and "
+  KEY_grave,    // Grave accent and tilde
+  KEY_comma,    // , (and <)
+  KEY_dot,      // . (and >)
+  KEY_slash,    // / (and ?)
+  KEY_cpslck,   // capslock
+  KEY_F1,
+  KEY_F2,
+  KEY_F3,
+  KEY_F4,
+  KEY_F5,
+  KEY_F6, 
+  KEY_F7,       // 0x40
+  KEY_F8,
+  KEY_F9,
+  KEY_F10,
+  KEY_F11,
+  KEY_F12,
+  KEY_PrtScr,
+  KEY_scrlck,
+  KEY_break,
+  KEY_ins,
+  KEY_home,
+  KEY_pgup,
+  KEY_del,
+  KEY_end,
+  KEY_pgdn,
+  KEY_rarr, 
+  KEY_larr,     // 0x50
+  KEY_darr,
+  KEY_uarr,
+  KEY_numlock,
+  KEY_KPslash,
+  KEY_KPast,
+  KEY_KPminus,
+  KEY_KPplus,
+  KEY_KPenter,
+  KEY_KP1,
+  KEY_KP2,
+  KEY_KP3,
+  KEY_KP4,
+  KEY_KP5,
+  KEY_KP6,
+  KEY_KP7,
+  KEY_KP8,      // 0x60
+  KEY_KP9,
+  KEY_KP0,
+  KEY_KPcomma,
+  KEY_Euro2,
+
+  /* These are NOT standard USB HID - handled specially in decoding,
+     so they will be mapped to the modifier byte in the USB report */
+  KEY_Modifiers,
+  MOD_LCTRL,    // 0x01
+  MOD_LSHIFT,   // 0x02
+  MOD_LALT,     // 0x04
+  MOD_LGUI,     // 0x08
+  MOD_RCTRL,    // 0x10
+  MOD_RSHIFT,   // 0x20
+  MOD_RALT,     // 0x40
+  MOD_RGUI,     // 0x80
+} keycode_t;
+
+typedef struct {
+    
+    port_t port;
+    uint8_t pin;
+    
+    bool_t debouncedState; //non-zero indicates that the button is being pressed.
+    int8_t cyclesRemaining;
+    
+    keycode_t key;
+    
+} button_t;
+
+#endif
