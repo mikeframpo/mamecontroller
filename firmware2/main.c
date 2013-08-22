8a2e01fa14218c74e652421a740061470a45d318
Mike Frampton <michael.frampton@pg.canterbury.ac.nz>
2013-08-22 20:06:14 +1200

    joystick can now send two separate reports for the two different players. At the moment only the p2 joystick is being sent back, need to figure out how to send the two reports.

--- firmware2/main.c
+++ firmware2/main.c
@@ -34,20 +34,23 @@ button_t gp1_buttons[] = {
     CREATE_BUTTON(PORT_A, (1 << 6), GP_BUT_F),
 
     CREATE_BUTTON(PORT_D, (1 << 0), GP_BUT_START),
+};
 
-    //CREATE_BUTTON(PORT_B, P2_UP),
-    //CREATE_BUTTON(PORT_B, P2_DOWN),
-    //CREATE_BUTTON(PORT_B, P2_LEFT),
-    //CREATE_BUTTON(PORT_B, P2_RIGHT),
-    //
-    //CREATE_BUTTON(PORT_A, P2_START),
-    //
-    //CREATE_BUTTON(PORT_C, P2_A),
-    //CREATE_BUTTON(PORT_C, P2_B),
-    //CREATE_BUTTON(PORT_C, P2_C),
-    //CREATE_BUTTON(PORT_C, P2_D),
-    //CREATE_BUTTON(PORT_C, P2_E),
-    //CREATE_BUTTON(PORT_C, P2_F),
+button_t gp2_buttons[] = {
+
+    CREATE_BUTTON(PORT_B, (1 << 3), GP_UP),
+    CREATE_BUTTON(PORT_B, (1 << 0), GP_DOWN),
+    CREATE_BUTTON(PORT_B, (1 << 1), GP_LEFT),
+    CREATE_BUTTON(PORT_B, (1 << 2), GP_RIGHT),
+
+    CREATE_BUTTON(PORT_C, (1 << 1), GP_BUT_A),
+    CREATE_BUTTON(PORT_C, (1 << 3), GP_BUT_B),
+    CREATE_BUTTON(PORT_C, (1 << 5), GP_BUT_C),
+    CREATE_BUTTON(PORT_C, (1 << 2), GP_BUT_D),
+    CREATE_BUTTON(PORT_C, (1 << 4), GP_BUT_E),
+    CREATE_BUTTON(PORT_C, (1 << 6), GP_BUT_F),
+
+    CREATE_BUTTON(PORT_A, (1 << 7), GP_BUT_START),
 };
 
 gamepad_t gp1 = {
@@ -56,12 +59,43 @@ gamepad_t gp1 = {
     .num_buttons = sizeof(gp1_buttons) / sizeof(gp1_buttons[0]),
 };
 
+gamepad_t gp2 = {
+    .id = 2,
+    .buttons = gp2_buttons,
+    .num_buttons = sizeof(gp2_buttons) / sizeof(gp2_buttons[0]),
+};
+
 const PROGMEM char usbHidReportDescriptor[] = {
     0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
     0x09, 0x04,                    // USAGE (Joystick)
     0xa1, 0x01,                    // COLLECTION (Application)
     0x09, 0x01,                    //   USAGE (Pointer)
     0xa1, 0x00,                    //   COLLECTION (Physical)
+    0x85, 0x01,                    //     REPORT_ID(1)
+    0x09, 0x30,                    //     USAGE (X)
+    0x09, 0x31,                    //     USAGE (Y)
+    0x15, 0x81,                    //     LOGICAL_MINIMUM (-127)
+    0x25, 0x7f,                    //     LOGICAL_MAXIMUM (127)
+    0x75, 0x08,                    //     REPORT_SIZE (8)
+    0x95, 0x02,                    //     REPORT_COUNT (2)
+    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+    0x05, 0x09,                    //     USAGE_PAGE (Button)
+    0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
+    0x29, 0x08,                    //     USAGE_MAXIMUM (Button 8)
+    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
+    0x75, 0x01,                    //     REPORT_SIZE (1)
+    0x95, 0x08,                    //     REPORT_COUNT (8)
+    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+    0xc0,                          //     END_COLLECTION
+    0xc0,                           // END_COLLECTION
+
+    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+    0x09, 0x04,                    // USAGE (Joystick)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0x09, 0x01,                    //   USAGE (Pointer)
+    0xa1, 0x00,                    //   COLLECTION (Physical)
+    0x85, 0x02,                    //     REPORT_ID(2)
     0x09, 0x30,                    //     USAGE (X)
     0x09, 0x31,                    //     USAGE (Y)
     0x15, 0x81,                    //     LOGICAL_MINIMUM (-127)
@@ -81,7 +115,7 @@ const PROGMEM char usbHidReportDescriptor[] = {
     0xc0                           // END_COLLECTION
 };
 
-#define REPORT_SIZE 3 /* 1 bit per button, rounded up to the nearest byte. */
+#define REPORT_SIZE 4 /* 1 bit per button, rounded up to the nearest byte. */
 static uint8_t reportBuffer[REPORT_SIZE];    /* buffer for HID reports, extra 1 is for the modifier byte. */
 static uint8_t idleRate = 1;
 
@@ -129,29 +163,39 @@ static inline void resetCycles(button_t* button) {
     }
 }
 
-static inline void buildReport (button_t* button, uint8_t* reportBuffer) {
-    switch (button->btype) {
-        case GP_LEFT:
-            reportBuffer[0] = -127;
-            break;
-        case GP_RIGHT:
-            reportBuffer[0] = 127;
-            break;
-        case GP_UP:
-            reportBuffer[1] = 127;
-            break;
-        case GP_DOWN:
-            reportBuffer[1] = -127;
-            break;
-        default:
-            reportBuffer[2] |= (1 << button->btype);
+static void buildReport (gamepad_t* gp, uint8_t* reportBuffer) {
+
+    int iBut;
+    button_t* button;
+
+    memset(reportBuffer, 0, REPORT_SIZE);
+    reportBuffer[0] = gp->id;
+
+    for (iBut = 0; iBut < gp->num_buttons; iBut++) {
+        button = &gp->buttons[iBut];
+        if (button->debouncedState) {
+            switch (button->btype) {
+                case GP_LEFT:
+                    reportBuffer[1] = -127;
+                    break;
+                case GP_RIGHT:
+                    reportBuffer[1] = 127;
+                    break;
+                case GP_UP:
+                    reportBuffer[2] = 127;
+                    break;
+                case GP_DOWN:
+                    reportBuffer[2] = -127;
+                    break;
+                default:
+                    reportBuffer[3] |= (1 << button->btype);
+            }
+        }
     }
 }
 
-void debounceButtons(gamepad_t* gamepad, uint8_t* reportBuffer) {
+void debounceButtons(gamepad_t* gamepad) {
     uint8_t iButton;
-    memset(reportBuffer, 0, REPORT_SIZE);
-
     for (iButton = 0; iButton < gamepad->num_buttons; iButton++) {
 
         button_t* button = &gamepad->buttons[iButton];
@@ -165,15 +209,6 @@ void debounceButtons(gamepad_t* gamepad, uint8_t* reportBuffer) {
                 button->debouncedState = rawState;
                 resetCycles(button);
             }
-        }
-
-        // Generate the report.
-        if (button->debouncedState) {
-            // [0]  report id
-            // [1]  x pos 0-255
-            // [2]  y pos 0-255
-            // [3]  [0..5] buttons, [6] start, [7] coin/quit.
-            buildReport(button, reportBuffer);
         }
     }
 }
@@ -269,6 +304,7 @@ int main(void) {
     memset(reportBuffer, 0, sizeof(reportBuffer));
 
     initButtons(&gp1);
+    initButtons(&gp2);
 
     while(1) {
         wdt_reset();
@@ -276,11 +312,13 @@ int main(void) {
 
         if (TCNT1 > 1200) { //1200 = 100us
             TCNT1 = 0;
-            debounceButtons(&gp1, reportBuffer);
+            debounceButtons(&gp1);
+            debounceButtons(&gp2);
         }
 
         if (TCNT0 > 47) { //47 == 4ms approx
             TCNT0 = 0;
+            buildReport(&gp2, reportBuffer);
             if(usbInterruptIsReady()) {
                 usbSetInterrupt(reportBuffer, sizeof(reportBuffer));
             }
